151de4e96d5aa68721e31a2964c6cb6913595dda
metarth <krystian.jasionek@gmail.com>
2024-10-09 10:44:10 +0200

    A

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
     css: ["@/assets/css/main.css"],
     modules: ["@pinia/nuxt"],
     app: {
-        baseURL: "/nuxt3-pwa/",
+        baseURL: "/nuxt3-pwa",
     },
     runtimeConfig: {
         public: {
